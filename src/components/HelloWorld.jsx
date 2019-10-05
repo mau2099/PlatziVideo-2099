05bb9ec9a02c218f6d2d6129d714ed239579c4ad
Mauricio Garcia <mau200729@gmail.com>
2019-10-05 03:20:40 -0500

    agregan componentes y estilos, se agregan carpetas static

--- src/components/HelloWorld.jsx
+++ src/components/HelloWorld.jsx
@@ -1,5 +1,6 @@
 import React from 'react';
 import '../assets/styles/App.scss';
+import '../assets/styles/components/HelloWorld.scss';
 const HelloWorld = () => (
   // eslint-disable-next-line no-unused-expressions
   <h1>Hola mundo</h1>
