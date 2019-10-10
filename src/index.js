ad0f2bd1f736e217310ff5e2e972e5030283293f
Mauricio Garcia <mau200729@gmail.com>
2019-10-10 18:24:19 -0500

    correccion de EsLint

--- src/index.js
+++ src/index.js
@@ -1,7 +1,7 @@
 import React from "react";
 import ReactDom from "react-dom";
 import { Provider } from "react-redux";
-import { CreateStore, createStore } from "redux";
+import { createStore } from "redux";
 import reducer from "./reducers";
 import App from "./routes/App";
 
