94e3d3dc753fde182e29268dca2ee0e7787a6da6
Mauricio Garcia <mau200729@gmail.com>
2019-10-14 12:58:57 -0500

    creacion de formulario Register con useState

--- src/components/Register.jsx
+++ src/components/Register.jsx
@@ -1,19 +1,59 @@
-import React from 'react';
-import '../assets/styles/components/Register.scss';
+import React, { useState } from "react";
+import "../assets/styles/components/Register.scss";
 
-const Register = () => (
-  <section className='register'>
-    <section className='register__container'>
-      <h2>Regístrate</h2>
-      <form className='register__container--form'>
-        <input className='input' type='text' placeholder='Nombre' />
-        <input className='input' type='text' placeholder='Correo' />
-        <input className='input' type='password' placeholder='Contraseña' />
-        <button className='button' type='button'>Registrarme</button>
-      </form>
-      <a href='/login'>Iniciar sesión</a>
+const Register = () => {
+  const [form, setValues] = useState({
+    email: "",
+    name: "",
+    password: "",
+  });
+
+  const handleInput = (event) => {
+    setValues({
+      ...form,
+      [event.target.name]: event.target.value,
+    });
+  };
+
+  const handleSubmit = (event) => {
+    event.preventDefault();
+    console.log(form);
+  };
+
+  return (
+    <section className="register">
+      <section className="register__container">
+        <h2>Regístrate</h2>
+        <form className="register__container--form" onSubmit={handleSubmit}>
+          <input
+            name="name"
+            className="input"
+            type="text"
+            placeholder="Nombre"
+            onChange={handleInput}
+          />
+          <input
+            name="email"
+            className="input"
+            type="text"
+            placeholder="Correo"
+            onChange={handleInput}
+          />
+          <input
+            name="password"
+            className="input"
+            type="password"
+            placeholder="Contraseña"
+            onChange={handleInput}
+          />
+          <button className="button" type="submit">
+            Registrarme
+          </button>
+          <a href="/login">Iniciar sesión</a>
+        </form>
+      </section>
     </section>
-  </section>
-);
+  );
+};
 
 export default Register;
